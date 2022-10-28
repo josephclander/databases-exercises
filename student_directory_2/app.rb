75bfaf961803365ceada96984a4de218244dbbd7
Joseph Lander <josephclander@gmail.com>
2022-10-28 20:16:48 +0100

    Add phase03 2-exercise app sol

--- student_directory_2/app.rb
+++ student_directory_2/app.rb
@@ -1,15 +1,16 @@
 # file: app.rb
 
 require_relative 'lib/database_connection'
+require_relative 'lib/cohort_repository'
 
-# We need to give the database name to the method `connect`.
 DatabaseConnection.connect('student_directory_2')
 
-# Perform a SQL query on the database and get the result set.
-sql = 'SELECT id, title FROM albums;'
-result = DatabaseConnection.exec_params(sql, [])
+cohort_repository = CohortRepository.new
+cohort2 = cohort_repository.find_with_students(2)
 
-# Print out each record from the result set .
-result.each do |record|
-  p record
+puts "Cohort Name: #{cohort2.cohort_name}"
+puts "Start Date: #{cohort2.start_date}"
+puts 'Students:'
+cohort2.students.each do |student|
+  puts "* #{student.student_name}"
 end
